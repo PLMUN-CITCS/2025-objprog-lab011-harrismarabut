d08bad1ee63c9abaa56fe43a660baee520abc7cc
Harrismarabut1 <marabutharris_bsit@plmun.edu.ph>
2025-02-23 23:28:07 +0800

    Update and rename Main (2).java to NumberClassifierMenu.java

--- NumberClassifierMenu.java
+++ NumberClassifierMenu.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class NumberClassifierMenu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char continueChoice;
